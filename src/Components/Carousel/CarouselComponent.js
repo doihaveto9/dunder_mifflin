4e3f0ba9929e87ac997307be4f2669b54b745978
Caleb Board <boardcaleb@yahoo.com>
2026-01-12 11:03:23 -0600

    Began implementing Employee component. Added employee listings

--- src/Components/Carousel/CarouselComponent.js
+++ src/Components/Carousel/CarouselComponent.js
@@ -1,41 +1,47 @@
 import { Carousel } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
+
 import logo from '../../Assets/dunderMifflinLogo.png';
 import image_one from '../../Assets/CarouselImage1.png';
 import image_two from '../../Assets/CarouselImage2.png';
 import image_three from '../../Assets/CarouselImage3.png';
 
 function CarouselComponent() {
+    const navigate = useNavigate();
+
     return (
       <div style={{ width: '100%', maxWidth: '888px', height: '500px', borderRadius: '20px' }}>
         <Carousel style={{ height: 'auto' }}>
-          <Carousel.Item interval={1500}>
-            <img
-              className="d-block w-100"
-              src={image_one}
-              alt="Image One"
-              style={{ borderRadius: '20px', objectFit: 'contain' }}
-            />
-            <Carousel.Caption style={{
-              position: 'absolute', 
-              bottom: 0, 
-              left: 0, 
-              right: 0, 
-              margin: 0,
-              padding: 0
-            }}>
-              <div style={{ 
-                width: '100%', 
-                background: "rgba(0,0,0,0.7)", 
-                padding: '20px', 
-                margin: 0, 
-                borderBottomLeftRadius: '20px',
-                borderBottomRightRadius: '20px'
+            <Carousel.Item onClick={() => navigate("/Employees")} interval={1500}
+              style={{ cursor: 'pointer' }}>
+              <img
+                className="d-block w-100"
+                src={image_one}
+                alt="Image One"
+                style={{ borderRadius: '20px', objectFit: 'contain' }}
+              />
+              <Carousel.Caption style={{
+                position: 'absolute', 
+                bottom: 0, 
+                left: 0, 
+                right: 0, 
+                margin: 0,
+                padding: 0
               }}>
-                <h3>Meet our lovely employees!</h3>
-              </div>
-            </Carousel.Caption>
-          </Carousel.Item>
-          <Carousel.Item interval={1500}>
+                <div style={{ 
+                  width: '100%', 
+                  background: "rgba(0,0,0,0.7)", 
+                  padding: '20px', 
+                  margin: 0, 
+                  borderBottomLeftRadius: '20px',
+                  borderBottomRightRadius: '20px'
+                }}>
+                  <h3>Meet our lovely employees!</h3>
+                </div>
+              </Carousel.Caption>
+            </Carousel.Item>
+          <Carousel.Item onClick={() => navigate("/Documentary")} interval={1500}
+            style={{ cursor: 'pointer' }}>
             <img
               className="d-block w-100"
               src={image_two}
@@ -62,7 +68,8 @@ function CarouselComponent() {
               </div>
             </Carousel.Caption>
           </Carousel.Item>
-          <Carousel.Item interval={1500}>
+          <Carousel.Item onClick={() => navigate("/Contact")} interval={1500}
+            style={{ cursor: 'pointer' }}>
             <img
               className="d-block w-100"
               src={image_three}
--- src/Components/Employees/Employees.js
+++ src/Components/Employees/Employees.js
@@ -1,7 +1,102 @@
+import MichaelScott from '../../Assets/EmployeeImages/MichaelScott.png';
+import PamBeesly from '../../Assets/EmployeeImages/PamBeesly.png';
+import DwightShrute from '../../Assets/EmployeeImages/DwightShrute.png';
+import JimHalpert from '../../Assets/EmployeeImages/JimHalpert.png';
+
+
+import './Employees.css';
+
 function Employees(){
+    const employees = [
+        {
+            id: 1,
+            image: MichaelScott,
+            quote: '"People say I am the best boss."',
+            name: "Michael Scott"
+        },
+        {
+            id: 2,
+            image: DwightShrute,
+            quote: '"Whenever I’m about to do something, I think, ‘Would an idiot do that?’ And if they would, I do not do that thing."',
+            name: "Dwight Schrute"
+        },
+        {
+            id: 3,
+            image: JimHalpert,
+            quote: '"From time to time, I send Dwight faxes… from himself… from the future."',
+            name: "Jim Halpert"
+        },
+        {
+            id: 4,
+            image: PamBeesly,
+            quote: '"There’s a lot of beauty in ordinary things. Isn’t that kind of the point?"',
+            name: "Pam Beesly"
+        },
+        {
+            id: 5,
+            image: PamBeesly,
+            quote: '"I Don\'t Need A Judge To Tell Me To Keep My Community Clean"',
+            name: "Ryan Howard"
+        },
+        {
+            id: 6,
+            image: PamBeesly,
+            quote: '"There’s a lot of beauty in ordinary things. Isn’t that kind of the point?"',
+            name: "Andy Bernard"
+        },
+        {
+            id: 7,
+            image: PamBeesly,
+            quote: '"There’s a lot of beauty in ordinary things. Isn’t that kind of the point?"',
+            name: "Stanley Hudson"
+        },
+        {
+            id: 8,
+            image: PamBeesly,
+            quote: '"There’s a lot of beauty in ordinary things. Isn’t that kind of the point?"',
+            name: "Kevin Malone"
+        },
+        {
+            id: 9,
+            image: PamBeesly,
+            quote: '"There’s a lot of beauty in ordinary things. Isn’t that kind of the point?"',
+            name: "Meredith Palmer"
+        },
+        {
+            id: 10,
+            image: PamBeesly,
+            quote: '"There’s a lot of beauty in ordinary things. Isn’t that kind of the point?"',
+            name: "Angela Martin"
+        },
+    ];
+
     return(
-        <div>
-            This is the Employees component
+        <div className="employeeContainer">
+            <div style={{ height: '2rem' }} />
+            {employees.map(employee => (
+                <div key={employee.id} className="employeeListing">
+                    <div className="employeeImage">
+                        <img 
+                            src={employee.image} 
+                            alt={employee.name}
+                        />
+                    </div>
+                    <div className="employeeInfo">
+                        <h2 style={{ 
+                            margin: '0'
+                        }}>
+                            {employee.name}
+                        </h2>
+                        <h2 style={{ 
+                            fontStyle: 'italic',
+                            margin: '0 0 10px 0'
+                        }}>
+                            {employee.quote}
+                        </h2>
+                    </div>
+                </div>
+            ))}
+            <div style={{ height: '2rem' }} />
         </div>
     )
 }
